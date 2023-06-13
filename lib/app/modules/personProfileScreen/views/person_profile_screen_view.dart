b6b91d040a64bc1d749b358dd813208769ed1f24
Bhoomi Rajani <75108835+BRRjani@users.noreply.github.com>
2023-06-13 23:19:41 +0530

    tab bar

--- lib/app/modules/personProfileScreen/views/person_profile_screen_view.dart
+++ lib/app/modules/personProfileScreen/views/person_profile_screen_view.dart
@@ -60,259 +60,260 @@ class _PersonProfileScreenViewState extends State<PersonProfileScreenView>
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 15.h,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 15.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 17.w, right: 40.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                StatusView(
+                  radius: 37.8.r,
+                  spacing: 7.r,
+                  strokeWidth: 3.w,
+                  indexOfSeenStatus: 0,
+                  numberOfStatus: 1,
+                  padding: 4.w,
+                  seenColor: ColorPicker.redColor,
+                  unSeenColor: ColorPicker.redColor,
+                  centerImageUrl: "https://picsum.photos/200/300",
+                ),
+                SizedBox(
+                  width: 25.w,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "1,234",
+                      style: TextStyle(
+                          color: ColorPicker.blackColor,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.sp),
+                    ),
+                    Text(
+                      "Stories",
+                      style: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          color: ColorPicker.blackColor),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 24.w,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "5,634",
+                      style: TextStyle(
+                          color: ColorPicker.blackColor,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.sp),
+                    ),
+                    Text(
+                      "Followers",
+                      style: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          color: ColorPicker.blackColor),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 24.w,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "9,202",
+                      style: TextStyle(
+                          color: ColorPicker.blackColor,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.sp),
+                    ),
+                    Text(
+                      "Following",
+                      style: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w400,
+                          color: ColorPicker.blackColor),
+                    )
+                  ],
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 17.w, right: 40.w),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  StatusView(
-                    radius: 37.8.r,
-                    spacing: 7.r,
-                    strokeWidth: 3.w,
-                    indexOfSeenStatus: 0,
-                    numberOfStatus: 1,
-                    padding: 4.w,
-                    seenColor: ColorPicker.redColor,
-                    unSeenColor: ColorPicker.redColor,
-                    centerImageUrl: "https://picsum.photos/200/300",
-                  ),
-                  SizedBox(
-                    width: 25.w,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "1,234",
-                        style: TextStyle(
-                            color: ColorPicker.blackColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 14.sp),
-                      ),
-                      Text(
-                        "Stories",
-                        style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                            color: ColorPicker.blackColor),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    width: 24.w,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "5,634",
-                        style: TextStyle(
-                            color: ColorPicker.blackColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 14.sp),
-                      ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                            color: ColorPicker.blackColor),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    width: 24.w,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "9,202",
-                        style: TextStyle(
-                            color: ColorPicker.blackColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 14.sp),
-                      ),
-                      Text(
-                        "Following",
-                        style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                            color: ColorPicker.blackColor),
-                      )
-                    ],
+          ),
+          SizedBox(
+            height: 8.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 17.w, right: 40.w),
+            child: Text(
+              "Username",
+              style: TextStyle(
+                  color: ColorPicker.blackColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 13.sp),
+            ),
+          ),
+          SizedBox(
+            height: 4.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 17.w, right: 20.w),
+            child: RichText(
+              textAlign: TextAlign.start,
+              text: TextSpan(
+                text:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
+                style: TextStyle(
+                    color: ColorPicker.blackColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12.sp),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '#hashtag',
+                    style: TextStyle(
+                        color: ColorPicker.linkColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12.sp),
                   ),
                 ],
               ),
             ),
-            SizedBox(
-              height: 8.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 17.w, right: 20.w),
+            child: Text(
+              "Link goes here",
+              style: TextStyle(
+                  color: ColorPicker.linkColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 12.sp),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 17.w, right: 40.w),
-              child: Text(
-                "Username",
-                style: TextStyle(
-                    color: ColorPicker.blackColor,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 13.sp),
-              ),
-            ),
-            SizedBox(
-              height: 4.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 17.w, right: 20.w),
-              child: RichText(
-                textAlign: TextAlign.start,
-                text: TextSpan(
-                  text:
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ",
-                  style: TextStyle(
-                      color: ColorPicker.blackColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 12.sp),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '#hashtag',
-                      style: TextStyle(
-                          color: ColorPicker.linkColor,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12.sp),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 17.w, right: 20.w),
-              child: Text(
-                "Link goes here",
-                style: TextStyle(
-                    color: ColorPicker.linkColor,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 12.sp),
-              ),
-            ),
-            SizedBox(
-              height: 16.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 17.w, right: 17.w),
-              child: ButtonWidget(
-                  context: context,
-                  fontSize: 14.sp,
-                  title: "Follow",
-                  onPressed: () {},
-                  height: 38.h,
-                  width: double.infinity,
-                  textColor: ColorPicker.blackColor,
-                  bgColor: ColorPicker.pureWhiteColor,
-                  redius: 8.r),
-            ),
-            SizedBox(
-              height: 13.h,
-            ),
-            TabBar(
-              physics: NeverScrollableScrollPhysics(),
-              controller: tabController,
-              indicatorWeight: 2.h,
+          ),
+          SizedBox(
+            height: 16.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 17.w, right: 17.w),
+            child: ButtonWidget(
+                context: context,
+                fontSize: 14.sp,
+                title: "Follow",
+                onPressed: () {},
+                height: 38.h,
+                width: double.infinity,
+                textColor: ColorPicker.blackColor,
+                bgColor: ColorPicker.pureWhiteColor,
+                redius: 8.r),
+          ),
+          SizedBox(
+            height: 13.h,
+          ),
+          TabBar(
+            physics: NeverScrollableScrollPhysics(),
+            controller: tabController,
+            indicatorWeight: 2.h,
 
-              // indicator: BoxDecoration(
-              //     borderRadius: BorderRadius.circular(50), // Creates border
-              //     color: ColorPicker.blackColor),
-              indicatorSize: TabBarIndicatorSize.tab,
-              unselectedLabelColor: ColorPicker.greyColor,
-              labelColor: Colors.black,
-              unselectedLabelStyle: TextStyle(
-                  fontSize: 14.sp,
-                  color: Color(0xff00000),
-                  fontWeight: FontWeight.w700),
-              indicatorColor: ColorPicker.blackColor,
-              labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14.sp,
-                  fontWeight: FontWeight.w700),
-              tabs: [
-                Tab(
-                  icon: ImageIcon(
-                      AssetImage(
-                        ImagePickerImage.storiesTab,
-                      ),
-                      size: 27.sp),
-                  child: Text(
-                    "Stories",
-                  ),
-                  // text: 'My Lead',
+            // indicator: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(50), // Creates border
+            //     color: ColorPicker.blackColor),
+            indicatorSize: TabBarIndicatorSize.tab,
+            unselectedLabelColor: ColorPicker.greyColor,
+            labelColor: Colors.black,
+            unselectedLabelStyle: TextStyle(
+                fontSize: 14.sp,
+                color: Color(0xff00000),
+                fontWeight: FontWeight.w700),
+            indicatorColor: ColorPicker.blackColor,
+            labelStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w700),
+            tabs: [
+              Tab(
+                icon: ImageIcon(
+                    AssetImage(
+                      ImagePickerImage.storiesTab,
+                    ),
+                    size: 27.sp),
+                child: Text(
+                  "Stories",
                 ),
-                Tab(
-                  icon: ImageIcon(
-                      AssetImage(
-                        ImagePickerImage.draftTab,
-                      ),
-                      size: 27.sp),
-                  child: Text(
-                    "Draft",
-                  ),
-                  // text: 'My Lead',
+                // text: 'My Lead',
+              ),
+              Tab(
+                icon: ImageIcon(
+                    AssetImage(
+                      ImagePickerImage.draftTab,
+                    ),
+                    size: 27.sp),
+                child: Text(
+                  "Draft",
                 ),
-                Tab(
-                  text: "Saved",
-                  icon: ImageIcon(
-                      AssetImage(
-                        ImagePickerImage.saveTab,
-                      ),
-                      size: 27.sp),
-                ),
-              ],
-            ),
-            // GetBuilder<PersonProfileScreenController>(
-            //   builder: (controller) =>
-            //       Expanded(
-            //     child: TabBarView(
-            //       physics: NeverScrollableScrollPhysics(),
-            //       controller: tabController,
-            //       children: [
-            //         //Screen1(),
-            //         Container(color: Colors.amber,height: 100,),
-            //         Container(color: Colors.amber,height: 100,),
-            //         Container(color: Colors.amber,height: 100,),
-            //
-            //         //Screen2()
-            //         //Screen3()
-            //       ],
-            //     ),
-            //   ),
-            // ),
-            TabBarView(
+                // text: 'My Lead',
+              ),
+              Tab(
+                text: "Saved",
+                icon: ImageIcon(
+                    AssetImage(
+                      ImagePickerImage.saveTab,
+                    ),
+                    size: 27.sp),
+              ),
+            ],
+          ),
+          // GetBuilder<PersonProfileScreenController>(
+          //   builder: (controller) =>
+          //       Expanded(
+          //     child: TabBarView(
+          //       physics: NeverScrollableScrollPhysics(),
+          //       controller: tabController,
+          //       children: [
+          //         //Screen1(),
+          //         Container(color: Colors.amber,height: 100,),
+          //         Container(color: Colors.amber,height: 100,),
+          //         Container(color: Colors.amber,height: 100,),
+          //
+          //         //Screen2()
+          //         //Screen3()
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          Expanded(
+            child: TabBarView(
                 controller: tabController,
                 children: [
 
-              Column(children: [Text("data"),],),
-              Column(children: [Text("data1"),],),
-              Column(children: [Text("data2"),],),
-            ])
-            // Expanded(
-            //   child: TabBarView(
-            //     physics: NeverScrollableScrollPhysics(),
-            //     controller: tabController,
-            //     children: [
-            //       //Screen1(),
-            //       Expanded(child: Container(color: Colors.amber,height: 100,)),
-            //       Expanded(child: Container(color: Colors.blueGrey,height: 100,)),
-            //       Expanded(child: Container(color: Colors.blue,height: 100,)),
-            //
-            //       //Screen2()
-            //       //Screen3()
-            //     ],
-            //   ),
-            // ),
-          ],
-        ),
+              Expanded(child: Column(children: [Text("data1"),],)),
+              Expanded(child: Column(children: [Text("data2"),],)),
+              Expanded(child: Column(children: [Text("data3"),],)),
+
+            ]),
+          )
+          // Expanded(
+          //   child: TabBarView(
+          //     physics: NeverScrollableScrollPhysics(),
+          //     controller: tabController,
+          //     children: [
+          //       //Screen1(),
+          //       Expanded(child: Container(color: Colors.amber,height: 100,)),
+          //       Expanded(child: Container(color: Colors.blueGrey,height: 100,)),
+          //       Expanded(child: Container(color: Colors.blue,height: 100,)),
+          //
+          //       //Screen2()
+          //       //Screen3()
+          //     ],
+          //   ),
+          // ),
+        ],
       ),
     );
   }
