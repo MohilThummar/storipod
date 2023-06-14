915732608d9a1de92865fae1773b18dc226a5d51
Bhoomi Rajani <75108835+BRRjani@users.noreply.github.com>
2023-06-14 23:56:48 +0530

    design done

--- lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart
+++
@@ -1,3 +0,0 @@
-export 'package:storipod_app/infrastructure/navigation/bindings/controllers/engagement.controller.binding.dart'; 
-export 'package:storipod_app/infrastructure/navigation/bindings/controllers/notification.controller.binding.dart'; 
-export 'package:storipod_app/infrastructure/navigation/bindings/controllers/reach_screen.controller.binding.dart'; 
